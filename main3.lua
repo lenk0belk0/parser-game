0e4ee9ba0ebc527b041a93191ea2b05ee2b4d648
Elena Anisimova <el.n.anisimova@yandex.ru>
2022-04-26 17:25:59 +0700

    Fix code formatting

--- main3.lua
+++ main3.lua
@@ -8,9 +8,20 @@ require "parser/mp-ru"
 
 fmt.para = true;
 
+window = Class {
+    -"окно";
+    disp = "окно";
+    description = [[ Ничего особенного. ]];
+    before_Enter = [[ Лучше этого не делать. ]];
+} : attr 'static, enterable'
+
+flor = Class {
+    -"пол";
+    disp = "пол";
+} : attr 'scenery, supporter'
 
 obj {
-	-"телефон|сотовый";
+    -"телефон|сотовый";
     nam = "phone";
     disp = "сотовый";
     description = [[ Сотовый телефон. Он выключен. ]];
@@ -18,61 +29,46 @@ obj {
 }
 
 obj {
-	-"ручка";
+    -"ручка";
     nam = "pen";
     disp = "ручка";
     description = [[ Обычная шариковая ручка с черными чернилами. Нужна чтобы закончить игру.]];
     after_Take = function(s)
         move(me(), 'final')
+        return false
     end;
 }
 
-window = Class {
-	-"окно";
-	disp = "окно";
-	description = [[ Ничего особенного. ]];
-	before_Enter = [[ Лучше этого не делать. ]];
-
-} : attr 'static, enterable'
-
-flor = Class {
-	-"пол";
-	disp = "пол";
-} : attr 'scenery, supporter'
-
 room {
-	-"комната";
-	nam = "main";
-	title = 'Комната';
-	disp = 'комната';
-	dsc = "Это небольшая комната";
-	obj = {
-		flor {
-			nam = 'main_flor';
-			obj = {
-				'pen'
-			};
-		}
-	};
-	after_Drop = function(s, w) 
-		move(w, 'main_flor')
-		return false;
-	end;
+    -"комната";
+    nam = "main";
+    title = 'Комната';
+    disp = 'комната';
+    dsc = "Это небольшая комната";
+    obj = {
+        flor {
+            nam = 'main_flor';
+            obj = { 'pen' };
+        }
+    };
+    after_Drop = function(s, w) 
+        move(w, 'main_flor')
+        return false;
+    end;
 } : with {
-	window {}
+    window {},
+    window {}
 }
 
 room {
-	nam = "final";
-	title = "Конец!";
-	dsc = 'Вот и всё.';
-	-- noparser = true;
+    nam = "final";
+    title = "Конец!";
+    dsc = 'Вот и всё.';
+    -- noparser = true;
 }
-
 
 function init()
-	-- Here will bi initialization
-	take('phone')
-	pl.word = -"ты/жр,2л"
-	pl.capacity = 1
+    take('phone')
+    pl.word = -"ты/жр,2л"
+    pl.capacity = 1
 end;
